bc6a68636a0e119162b9e9f6919259d4e9b7bab4
Nirmit18 <nimitkaundalrock2004@gmail.com>
2024-04-29 21:56:41 +0530

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+
+const page = () => {
+  return (
+    <nav className="flex justify-between">
+      <div>hello</div>
+      <div className="flex gap-2">
+        <div className="hidden sm:block">contact us</div>
+        <div>about us</div>
+      </div>
+      <div
+  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+/>
+    </nav>
+  )
+};
+
+export default page;
